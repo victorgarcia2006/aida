d59dfa4356c9a9fbf41bf0de8bb9c49b6cc36d9d
victorgarcia2006 <132796314+victorgarcia2006@users.noreply.github.com>
2025-06-13 21:02:20 -0600

    :sparkles: Functional Button

--- apps/mobile/lib/screens/home_screen.dart
+++ apps/mobile/lib/screens/home_screen.dart
@@ -2,6 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:mobile/widgets/header_screen.dart';
 import 'package:tabler_icons/tabler_icons.dart';
 import 'package:mobile/widgets/navbar_widget.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+Future<void> sendEmergencyMessage() async {
+  final url = Uri.parse("https://aiden-production.up.railway.app/twilio/send-message/");
+
+  final response = await http.post(
+    url,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: jsonEncode({
+      "body": "Soy Victor Garcia y mi ubicacion es 48.8566,2.3522"
+    }),
+  );
+
+  if (response.statusCode == 200) {
+    print("Mensaje enviado correctamente");
+  } else {
+    print("Error al enviar mensaje: ${response.statusCode}");
+  }
+}
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -20,7 +42,7 @@ class HomeScreen extends StatelessWidget {
             child: IconButton(
             style: ButtonStyle(backgroundColor: WidgetStatePropertyAll<Color>(
                               Color.fromRGBO(158, 150, 228, 1))),
-            onPressed: () => (),
+            onPressed: sendEmergencyMessage,
             icon: Icon(TablerIcons.alert_triangle, color: Colors.white, size: 200, weight: 0.2,)
             ),
           )
