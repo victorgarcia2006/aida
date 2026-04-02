889723e9fe383ceac8ca975240eaea6fe4261b65
victorgarcia2006 <vgarciac2100@alumno.ipn.mx>
2026-04-01 19:37:18 -0600

    :lock: Remove database link

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -5,9 +5,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { WhatsappModule } from './whats-app/whats-app.module';
 import { ConfigModule } from '@nestjs/config';
 import { TwilioModule } from './twilio/twilio.module'; // Adjust the import path as necessary
+import { configDotenv } from 'dotenv';
+
+configDotenv();
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://exporjames06:08Mayo_2006@aiden.tks3wq4.mongodb.net/?retryWrites=true&w=majority&appName=aiden'), WhatsappModule,
+  imports: [MongooseModule.forRoot(process.env.MONGODB_URI!), WhatsappModule,
     /* ConfigModule.forRoot({ isGlobal: true,
       envFilePath: '../.env', // Adjust
      }), */
